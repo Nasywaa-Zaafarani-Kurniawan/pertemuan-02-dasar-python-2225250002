747f3cb94d72276d15f0a3ebb6c6f29e1d66819d
Nasywaa Zaafarani Kurniawan <nasywaakurniawan@gmail.com>
2026-09-09 11:49:55 +0700

    Delete latihan/02_persegi_panjang.py

--- latihan/02_persegi_panjang.py
+++
@@ -1,9 +0,0 @@
-panjang = float(input("Masukkan panjang persegi panjang: "))
-lebar = float(input("Masukkan lebar persegi panjang: "))
-
-luas = panjang * lebar
-keliling = 2 * (panjang + lebar)
-
-print()
-print(f"Luas persegi panjang: {luas}")
-print(f"Keliling persegi panjang: {keliling}")
